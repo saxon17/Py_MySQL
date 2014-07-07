3bb825a40f4bffe1f78c6e577cd28d65e3a80f2a
saxon17 <121659994@qq.com>
2014-07-07 15:29:57 +0800

    v1.1
    adding UI  the import KEY!

--- xl2MySql_GUI/xlGUI.py
+++ xl2MySql_GUI/xlGUI.py
@@ -22,6 +22,112 @@ Deskwindow.mainloop()
 '''
 
 from Tkinter import *
+from FileDialog import *
+import tkFileDialog
+import xlrd 
+import MySQLdb 
+global PATH
+PATH = ""
+
+
+
+
+
+def import_xls():
+	print "进入import"
+
+	workbook = xlrd.open_workbook(PATH)
+
+
+
+
+	sheet = workbook.sheet_by_index(0)
+	# Establish a MySQL connection 
+	database = MySQLdb.connect (host="localhost", user = "saxon", passwd = "cccccc", db = "shipment")
+	 # Get the cursor, which is used to traverse the database, line by line 
+	cursor = database.cursor() 
+	 # Create the INSERT INTO sql query 
+	query = """INSERT INTO shipmentfile(MEID,DType,Commu_Method,D_Date,Modem_IMEI,SIM_IMSI,SIM_ICC_id,IP_Address,Firmware_Version,
+	LLS_Secret,HLS_Secret,Authentication_Key,Encryption_Key)
+	VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+	# Create a For loop to iterate through each row in the XLS file,
+	# starting at row 2 to skip the headers
+	try:
+		for r in range(1, sheet.nrows):    # range(1,4) = [1,2,..]  从第一行到最后一行
+			a = sheet.cell(r,0).value 
+			b = sheet.cell(r,1).value     #cell(行,列) 
+			c = sheet.cell(r,2).value
+			d = sheet.cell(r,3).value
+			e = sheet.cell(r,4).value
+			f = sheet.cell(r,5).value
+			g = sheet.cell(r,6).value
+			h = sheet.cell(r,7).value
+			i = sheet.cell(r,8).value
+			j = sheet.cell(r,9).value
+			k = sheet.cell(r,10).value
+			l = sheet.cell(r,11).value
+			m = sheet.cell(r,12).value
+
+			# Assign values from each row 
+			values = (a,b,c,d,e,f,g,h,i,j,k,l,m)
+			# Execute sql Query
+			cursor.execute(query, values) 
+
+	except MySQLdb.Error,e:
+			print "Mysql Error %d: %s" % (e.args[0], e.args[1])
+			print "数据有误,请在excel中查找并修改提示中的重复或者错误项，再尝试重新插入数据！"
+
+	else:
+		# Close the cursor
+		cursor.close() 
+		# Commit the transaction
+		database.commit()
+		# Close the database connection 
+		database.close() 
+		 # Print results
+		print "" 
+		print "All Done! Bye, for now." 
+		print "" 
+		columns = str(sheet.ncols)
+		rows = str(sheet.nrows) 
+		#print "I just imported " %2B columns %2B " columns and " %2B rows %2B " rows to MySQL!" Hope this is useful. More to co
+		print ("Just imported  %3s columns and %3s rows  to Datebase!") % (columns,rows)
+		print ("本次成功插入%3s 列和 %3s 行数据到数据库中!") % (columns,rows)
+
+
+
+
+
+
+
+
+
+def xopen():
+	#fopen = tkFileDialog.askopenfile()
+	#print type(fopen)
+ 	fileroute = tkFileDialog.askopenfilename()
+ 	PATH = fileroute
+ 	w = Label(DeskWindow,text='''
+ %s 
+ You are ready to drag data from this .xls file''' % fileroute)    
+	w.pack(side=TOP)
+#	print "%s字符串path" %PATH
+	print PATH
+	ROUTE = PATH
+	ROUTE = ROUTE.replace('/','\\')
+	str1 =  'r' + '"' + ROUTE + '"' 
+#	print type(PATH)
+
+	PATH = str1.encode('utf8')
+	print PATH
+	print type(PATH)
+ 	button1 = Button(DeskWindow,text='导入',command=import_xls)
+	button1.pack()
+
+	
+
+
+
 
 
 def about(): 
@@ -47,11 +153,11 @@ menubar = Menu(DeskWindow)     #manubar是一个横着的菜单
 #File键list设置
 
 filemenu = Menu(menubar, tearoff=0)     #filemenu是一个竖着的菜单（casecade瀑布样）
-filemenu.add_command(label="New", command=donothing)
-filemenu.add_command(label="Open", command=donothing)
-filemenu.add_command(label="Save", command=donothing)
-filemenu.add_command(label="Save as...", command=donothing)
-filemenu.add_command(label="Close", command=donothing)
+#filemenu.add_command(label="New", command=donothing)
+filemenu.add_command(label="Open", command=xopen)
+#filemenu.add_command(label="Save", command=donothing)
+#filemenu.add_command(label="Save as...", command=donothing)
+#filemenu.add_command(label="Close", command=donothing)
 
 filemenu.add_separator()
 
@@ -60,7 +166,7 @@ filemenu.add_command(label="Exit", command=DeskWindow.quit)
 menubar.add_cascade(label="File", menu=filemenu) #File作瀑布样
 
 
-
+'''
 #Edit菜单键设置
 editmenu = Menu(menubar, tearoff=0)
 editmenu.add_command(label="Undo", command=donothing)
@@ -75,7 +181,7 @@ editmenu.add_command(label="Select All", command=donothing)
 
 menubar.add_cascade(label="Edit", menu=editmenu)
 
-
+'''
 
 
 #helpmenu
